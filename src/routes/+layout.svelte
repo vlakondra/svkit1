3555669bbfe2c631159cfd84f8aedcca6a14d3b6
vlakondra <kondrashkin.wladimir@yandex.ru>
2025-03-18 17:33:26 +0500

    `Added Tailwind CSS dependencies and configurations`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
+    import "../app.css";
     import type { Snippet } from "svelte";
     import type { LayoutData } from "./$types";
-
+    
     let { data, children }: { data: LayoutData; children: Snippet } = $props();
 </script>
 
 <h4>Layout.svelte</h4>
-{JSON.stringify(data)}
-{#if data.rootData.val}
-    <h4>{data.rootData.val}</h4>
-{/if}
+
+
 <div>{@render children()}</div>
